5030b1d5ac6512e838af9540553d1a58d153553f
MallyMartinez <mallymartinez20@gmail.com>
2025-02-13 15:17:43 -0600

    JSX 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
-    const married = true
-    return <h1>{married ? "estoy casado": "no estoy casado"}</h1>
+  const married = true;
+  return <h1>{married ? "estoy casado" : "no estoy casado"}</h1>;
 }
-//self closing tags 
-root.render(<div>
+//self closing tags
+root.render(
+  <div>
     <Greeting />
-</div>)
+  </div>
+);
