e4ea4098d4a777d18c3995c4ff319ac1fc4d07fc
MallyMartinez <mallymartinez20@gmail.com>
2025-02-13 15:29:48 -0600

    booleano a string

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
-  const user = {
-    fistname: "mally",
-    lastname: "martinez"
-  }
+  const married = true 
   return <div>
-    <h1>{user.fistname}</h1>
-    <h1>{user.lastname}</h1>
+    <h1>{married.toString()}</h1>
     </div>
 }
 //self closing tags
